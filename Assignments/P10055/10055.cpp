f9ba185ccc1401d158bfaac001eff87e50c2c1ed
tracotr <124739591+tracotr@users.noreply.github.com>
2024-08-28 14:03:50 -0500

    changes

---
+++ Assignments/P10055/10055.cpp
@@ -0,0 +1,30 @@
+/**
+* Jaxon King
+* 4883
+* Sept. 5th 2024
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+    string fileName;
+    cout << "filename: ";
+    getline(cin, fileName);
+    fileName += ".txt";
+    cout << fileName;
+
+
+    long B = 0, A = 0;
+
+    while (cin >> A >> B) {
+
+        break;
+
+    }
+    return 0;
+}
