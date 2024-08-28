b4a436cba9b89663385ce98843629cd92435802b
tracotr <124739591+tracotr@users.noreply.github.com>
2024-08-28 15:17:54 -0500

    done

--- Assignments/P10055/10055.cpp
+++ Assignments/P10055/10055.cpp
@@ -12,19 +12,19 @@
 using namespace std;
 
 int main() {
-    string fileName;
-    cout << "filename: ";
-    getline(cin, fileName);
-    fileName += ".txt";
-    cout << fileName;
-
-
+    // Open the input file and output file so it can know what to use lol
+    ifstream i("uDebugInput.txt");
+    ofstream o("uDebugOutput.txt");
+    
     long B = 0, A = 0;
 
-    while (cin >> A >> B) {
-
-        break;
-
+    while (i >> A >> B) {
+        // if A is greater than B, get the difference of a and b, difference of b and a otherwise
+        long r = (A > B)? A - B : B - A; 
+        o << r << endl;
     }
+
+    i.close();
+    o.close();
     return 0;
 }
---
+++ Assignments/P10055/README.md
@@ -0,0 +1,17 @@
+## A03
+### Jaxon King
+### Description:
+
+onlinejudge.org problem 10055
+https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=996
+
+### Files
+
+|   #   | File             | Description                                        |
+| :---: | ---------------- | -------------------------------------------------- |
+|   1   | Main.cpp         | Main file with code to solve problem               |
+|   2   | uDebugInput.txt  | Input file with example cases                      |
+|   3   | uDebugOutput.txt | Output file of example cases                       |
+
+<img src="Capture.PNG" width="200">
+
