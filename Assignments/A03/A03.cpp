9c79911bd08671d6c784cc207cec42e39350ce1a
tracotr <124739591+tracotr@users.noreply.github.com>
2024-08-29 11:20:13 -0500

    Update A03.cpp

--- Assignments/A03/A03.cpp
+++ Assignments/A03/A03.cpp
@@ -4,8 +4,8 @@
 * Sept. 5th 2024
 */
 #include <iostream>
-#include <fstream>
-#include <string>
+//#include <fstream>
+
 
 #define endl "\n"
 
@@ -13,19 +13,18 @@ using namespace std;
 
 int main() {
     // Open the input file and output file so it can know what to use lol
-    ifstream i("uDebugInput.txt");
-    ofstream o("uDebugOutput.txt");
+    //ifstream i("uDebugInput.txt");
+    //ofstream o("uDebugOutput.txt");
     
     long long B = 0, A = 0; // long long because test cases go past int_max
 
-    while (i >> A >> B) {
+    while (cin >> A >> B) {
         // if A is greater than B, get the difference of a and b, difference of b and a otherwise
-        cout << A << " " << B << endl;
         long long r = (A > B)? A - B : B - A; 
-        o << r << endl;
+        cout << r << endl;
     }
 
-    i.close();
-    o.close();
+    //i.close();
+    //o.close();
     return 0;
 }
