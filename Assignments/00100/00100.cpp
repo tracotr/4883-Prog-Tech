b3e851dbaa0fffaf0729821d95c12edb5acc3dab
tracotr <124739591+tracotr@users.noreply.github.com>
2024-08-29 12:05:22 -0500

    finished problem 00100

---
+++ Assignments/00100/00100.cpp
@@ -0,0 +1,54 @@
+/**
+ * Jaxon King
+ * 4883
+ * 00100 The 3n + 1 Problem
+ * Dec. 6th 2024
+ */
+#include <iostream>
+//#include <fstream>
+
+#define endl "\n"
+
+using namespace std;
+
+int main()
+{
+    // Open the input file and output file so it can know what to use lol
+    //ifstream i("uDebugInput.txt");
+    //ofstream o("uDebugOutput.txt");
+
+    int B = 0, A = 0;
+
+    while (cin >> A >> B) {
+        // loop A -> B, keep track of highest score
+        // i is start so it doesn't affect it when I try to print
+        int start = (A < B)? A : B; // if a is less, a start, otherwise b end
+        int end  = (A < B)? B : A; // if a is less, b end, otherwise a end
+
+        int max_cycle = 1; // keeping track of the max cycle found
+        
+            for (start; start <= end; start++) {
+                // Continue algo until num == 1
+                int num = start;
+                int cycle = 1;
+                while (num != 1) { // until the number we're running is 1
+                    if (num % 2 == 0) // if even
+                        num /= 2;
+                    else 
+                        num = num * 3 + 1;
+
+                    cycle++;
+                }
+
+                if(cycle > max_cycle)
+                    max_cycle = cycle;
+            }
+
+        cout << A << " " << B << " " << max_cycle << endl;
+        max_cycle = 0;
+    }
+
+    //i.close();
+    //o.close();
+    return 0;
+}
