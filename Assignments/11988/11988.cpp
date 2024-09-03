8b850d8357314c49163957512db47d68d6d82176
tracotr <124739591+tracotr@users.noreply.github.com>
2024-09-03 11:59:48 -0500

    11988 FINISHED!

--- Assignments/11988/11988.cpp
+++ Assignments/11988/11988.cpp
@@ -4,6 +4,8 @@
  * 11988 Broken Keyboard (a.k.a. Beiju Text)
  */
 #include <iostream>
+#include <string>
+//#include <fstream>
 
 #define endl "\n"
 
@@ -11,6 +13,10 @@ using namespace std;
 
 int main()
 {
+    // Open the input file and output file so it can know what to use lol
+    //ifstream i("uDebugInput.txt");
+    //ofstream o("uDebugOutput.txt");
+
     string A = "";
 
     while(cin >> A){
@@ -18,14 +24,15 @@ int main()
         string result = ""; // result string to print
         string prepend = ""; // prepend to store chars to add into front of result
 
-        // this for loop super slow sooooo
-        // to fix probably just read directly to next [], remove everything before it, 
-        // sleepy so do later : )
 
         for(int i = 0; i < A.length(); i++){
             if(A[i] == '[' || A[i] == ']'){ // if home or end brackets
                 addBack = (A[i] == ']') ? true : false; // if end add to back, if home add to front
-                result = prepend + result; // add current prepend to front of result
+                result.insert(0, prepend); // add current prepend to front of result
+                // learning moment:
+                // result = result + prepend
+                // is MUCH slower for some reason, rather than insert.
+                // probably because optimizations in the library 
                 prepend = ""; // clear bc need to :)
                 continue; // stop loop because we dont need to add to result
             }
@@ -39,5 +46,7 @@ int main()
         cout << result << endl;
     }
 
+    //i.close();
+    //o.close();
     return 0;
 }
---
+++ Assignments/11988/README.md
@@ -0,0 +1,23 @@
+## 11988
+#### Jaxon King
+#### 4883
+#### Description:
+
+onlinejudge.org problem 11988
+https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=3139
+
+### Files
+
+|   #   | File             | Description                                        |
+| :---: | ---------------- | -------------------------------------------------- |
+|   1   | Main.cpp         | Main file with code to solve problem               |
+|   2   | uDebugInput.txt  | Input file with example cases                      |
+|   3   | uDebugOutput.txt | Output file of example cases                       |
+
+### Instructions
+- Should just work, I didn't add typing in an input name so any different input files will need to be changed in the code
+
+<img src="Capture.PNG" width="600">
+
+### Other
+- result.insert(0, prepend) is faster than result = prepend + result, sooo good to know
